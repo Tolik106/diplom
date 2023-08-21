bee7f6fd2f8f3dac7847aaa98a86785ce074c0a3
Анатолий Савин <beningtonchess@yandex.ru>
2023-08-21 21:28:04 +0300

    2

--- create_db.py
+++ create_db.py
@@ -1,23 +1,26 @@
 import psycopg2
 from psycopg2 import Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import os
 
-def create_db():
-    user = input(f' Введите имя пользователя в SQL')
-    password = input(f' Введите пароль пользователя в SQL')
+
+
+
+def create_db(USER, PASSWORD):
+
 
 
     try:
         # Подключение к существующей базе данных
-        connection = psycopg2.connect(user=user,
+        connection = psycopg2.connect(user=USER,
                                       # пароль, который указали при установке PostgreSQL
-                                      password=password,
+                                      password=PASSWORD,
                                       host="localhost",
                                       port="5432")
         connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         # Курсор для выполнения операций с базой данных
         cursor = connection.cursor()
-        sql_create_database = 'create database vkinder_db'
+        sql_create_database = 'create database vkinder_db_1'
         cursor.execute(sql_create_database)
     except (Exception, Error) as error:
         print("Ошибка при работе с PostgreSQL", error)
@@ -30,4 +33,4 @@ def create_db():
 if __name__ == '__main__':
 
 
-    create_db()
+    create_db(USER, PASSWORD)
--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,6 @@
 
 import sqlalchemy as sq
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base
 
 
 Base = declarative_base()
@@ -8,20 +8,12 @@ Base = declarative_base()
 
 
 
-
-class Last_person(Base):
-    __tablename__ = "last_person"
-    user_id = sq.Column(sq.Integer, primary_key=True)
-    person_id = sq.Column(sq.String(length=40), unique=True)
-
-
-
 class User(Base):
     __tablename__ = "user"
-    id = sq.Column(sq.Integer,sq.ForeignKey("last_person.user_id"), primary_key=True)
-    # last_person_id = sq.Column(sq.Integer, sq.ForeignKey("last_person.user_id"))
+    id = sq.Column(sq.Integer, primary_key=True)
+    last_person_id = sq.Column(sq.Integer, unique=True)
     page = sq.Column(sq.String(length=6), unique=True)
-    last_person = relationship("Last_person", backref="user", uselist=False)
+
 
 
 
@@ -35,12 +27,12 @@ class Favorites(Base):
 class Black_list(Base):
     __tablename__ = "black_list"
 
-
     user_id = sq.Column(sq.Integer(), sq.ForeignKey('user.id'), primary_key=True)
     person_id = sq.Column(sq.Integer, primary_key=True)
 
 
 
 def create_tables(engine):
-    Base.metadata.drop_all(engine)
+
+    # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
